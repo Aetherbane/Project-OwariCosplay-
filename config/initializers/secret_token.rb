e5f5183176ee2a4bdc16a6c71e2c795fd1475a6e
Allan Sechrist <abyssalspecter@gmail.com>
2014-01-24 15:51:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fumblr::Application.config.secret_key_base = 'b444103ff84496dcb27032f8d5239df681a3b29be5c30567186b08a46f7217ecc6e123e4d2f36d500bdedfa55a822b2fc1d93e3f2997c67a0a7a76d7e06be8aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fumblr::Application.config.session_store :cookie_store, key: '_Fumblr_session'
